0537caec5d2bb68cba1df3726b476ddfc5348b59
Den <degtyar87@mail.ru>
2021-04-22 15:55:06 +0600

    Version_1.1

--- app/src/main/java/ru/zanuda/myappgit/MainActivity.kt
+++ app/src/main/java/ru/zanuda/myappgit/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.databinding.DataBindingUtil
 import ru.zanuda.myappgit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var das: String = "qwerty" +
+            ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val bind : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
